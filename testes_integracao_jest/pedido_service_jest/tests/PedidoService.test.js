98037f017bf4e571ad644714e9ec74424e20e10e
ABILIO ALFREDO FRANCISCO <abilio_francisco@estudante.sesisenai.org.br>
2025-04-14 20:43:29 -0300

    Atualizações no test

--- testes_integracao_jest/pedido_service_jest/tests/PedidoService.test.js
+++ testes_integracao_jest/pedido_service_jest/tests/PedidoService.test.js
@@ -4,22 +4,20 @@ const PedidoService = require('../classes/PedidoService');
 const PagamentoService = require('../classes/PagamentoService');
 const NotificacaoService = require('../classes/NotificacaoService');
 
-describe('PedidoService', () => {
+describe('Teste de Pedido Service', () => {
 
-  test("pedido realizado com sucesso", () => {
-    // GIVEN
-    let pagamentoService = new PagamentoService();
-    let notificacaoService = new NotificacaoService();
-
+  test("Fechar pagamento com sucesso", () => {
+    // GIVEN - DADO
+    const pagamentoService = new PagamentoService();
     const mockPagamento = jest.spyOn(pagamentoService, "processarPagamento")
                               .mockReturnValue(true);
 
+    const notificacaoService = new NotificacaoService();
     const mockNotificacao = jest.spyOn(notificacaoService, "enviarNotificacao");
 
+    const pedidoService = new PedidoService(pagamentoService, notificacaoService);
 
-    let pedidoService = new PedidoService(pagamentoService, notificacaoService);
-
-    let pedidoDummy = {"pedido": 1}
+    let pedidoDummy = {id: 1}
 
     // WHEN
     const resultado = pedidoService.fecharPedido(pedidoDummy)
@@ -27,30 +25,35 @@ describe('PedidoService', () => {
     // THEN
     expect(resultado).toBeTruthy()
     expect(mockPagamento).toHaveBeenCalled()
-    expect(mockNotificacao).toHaveBeenCalled()
+    expect(mockNotificacao).toHaveBeenCalledWith(pedidoDummy)
+    // expect(mockNotificacao).toHaveBeenCalledTimeOut()
+    // expect(mockNotificacao).toHaveBeenCalled()
   })
 
   test("pedido realizado com falha", () => {
-    // GIVEN
-    let pagamentoService = new PagamentoService();
-    let notificacaoService = new NotificacaoService();
+    // GIVEN - DADO
+    const pagamentoService = new PagamentoService();
+    const notificacaoService = new NotificacaoService();
 
     const mockPagamento = jest.spyOn(pagamentoService, "processarPagamento")
                               .mockReturnValue(false);
 
     const mockNotificacao = jest.spyOn(notificacaoService, "enviarNotificacao");
 
+    const pedidoService = new PedidoService(pagamentoService, notificacaoService);
 
-    let pedidoService = new PedidoService(pagamentoService, notificacaoService);
+    const pedidoDummy = {id: 1}
 
-    let pedido = {"id": 1}
-
-    // WHEN
-    expect(() => {
-      pedidoService.fecharPedido(pedido)
-    }).toThrow(`Pedido ${pedido.id} não pôde ser fechado. Pagamento inválido.`)
+    // // WHEN
+    // expect(() => {
+    //   pedidoService.fecharPedido(pedido)
+    // }).toThrow(`Pedido ${pedido.id} não pôde ser fechado. Pagamento inválido.`)
 
     // THEN
+    expect(() => {
+      pedidoService.fecharPedido(pedidoDummy)
+    }).toThrow(`Pedido ${pedidoDummy.id} não pôde ser fechado. Pagamento inválido.`)
+
     expect(mockPagamento).toHaveBeenCalled()
     expect(mockNotificacao).not.toHaveBeenCalled()
   })
